0bb8bc8ced1b52916d8a58b0451175f3ae4b4632
mariellepamaran <mariellepamaran@gmail.com>
2021-12-22 12:03:33 +0800

    updated version number

--- utils/changestream.js
+++ utils/changestream.js
@@ -444,7 +444,7 @@ const connect = function(io,_ping_,ENVIRONMENT){ //io
     
             socket.emit("*",JSON.stringify({
                 type: "credentials",
-                version: "vv.-2.58.127.15",
+                version: "vv.-2.58.128.15",
                 forceUpdate: ["fleet"],
                 // ["coket1","coket2","wilcon"]
                 data: auth.getCredentials(dbName)
